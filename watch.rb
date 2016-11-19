7e7f560e8c43e1f4e2ce3f75f186bfa676c62ff5
David Balatero <dbalatero@gmail.com>
2016-11-18 17:28:34 -0800

    Fixes

--- watch.rb
+++ watch.rb
@@ -3,13 +3,13 @@ require 'bundler'
 
 Bundler.require
 
-if ARGV.size != 2
+if ARGV.size < 2
   abort "Usage: ruby watch.rb [sms number] [url]"
 end
 
 number = ARGV[0]
 url = ARGV[1]
-sleep_ms = ARGV[2] || 300
+sleep_ms = (ARGV[2] || 300).to_i
 
 def send_sms(number, message)
   system(
@@ -22,19 +22,22 @@ puts "==> Watching #{url}"
 send_sms(number, "Watching #{url} for changes...")
 
 previous_hash = nil
+previous_body = nil
 
 loop do
   content = HTTParty.get(url).body
   current_hash = Digest::MD5.hexdigest(content)
 
   print "Checking URL... got hash #{current_hash[0..6]}... "
-  print "old hash #{previous_hash[0..6]}... "
+  print "old hash #{previous_hash[0..6]}... " if previous_hash
 
   if previous_hash != current_hash && !previous_hash.nil?
+    print "sending text... "
     send_sms(number, "Page has changed! #{url}")
   end
 
   previous_hash = current_hash
+  previous_body = content
 
   puts "sleeping"
 
